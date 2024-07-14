227b546dad3e93ff2911b539e59596330b5b7558
teaisawesome <dekany.r123@gmail.com>
2024-07-14 23:19:40 +0200

    feat: studio threejs canvas added

--- src/app/studio/page.jsx
+++ src/app/studio/page.jsx
@@ -1,9 +1,17 @@
+'use client'
+
+import { Canvas } from '@react-three/fiber'
 import classes from './page.module.css'
 
 export default function Studio() {
     return (
         <>
             <h1 className={classes.title}>Studio Page Helloszia</h1>
+            <div className={classes.scene}>
+                <Canvas shadows className={classes.canvas} camera={{position: [-6, 7, 7]}}>
+
+                </Canvas>
+            </div>
         </>
     )
 }
