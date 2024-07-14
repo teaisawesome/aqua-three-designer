1944f66d767f2525d72f1c12071f8d0f8bca21ab
teaisawesome <dekany.r123@gmail.com>
2024-07-14 23:06:11 +0200

    feat: studio page created

---
+++ src/app/studio/page.jsx
@@ -0,0 +1,9 @@
+import classes from './page.module.css'
+
+export default function Studio() {
+    return (
+        <>
+            <h1 className={classes.title}>Studio Page Helloszia</h1>
+        </>
+    )
+}
