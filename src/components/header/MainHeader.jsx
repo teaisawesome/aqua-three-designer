d9503136c5de8df3c22925a8d4e3ec0408331ef1
dekrob <dekany.r123@gmail.com>
2024-09-17 23:58:20 +0200

    feat(main-header): small and medium-large device header sections separated

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -14,9 +14,11 @@ const MainHeader = () => {
     
     return (
         <header>
-            <nav className="bg-slate-500">
-                // TODO: need refactor md navbar should be separated from main div, same with sm logo and hamburger
-                <div className="flex md:items-center md:justify-center bg-red-500 md:w-max md:mx-auto md:p-3 md:rounded-full">
+            <nav>
+                { 
+                    // * medium-large device header
+                }
+                <div className="flex md:items-center md:justify-center mt-5 bg-sky-800 md:w-max md:mx-auto md:p-4 md:rounded-full drop-shadow-lg">
                     <div className="hidden md:flex space-x-4">
                         <div className="flex items-center md:justify-center font-semibold self-center">
                             <span>Aqua<b className="text-cyan-500">3D</b>esigner</span>
@@ -25,38 +27,45 @@ const MainHeader = () => {
                         <Link className="self-center" href="/studio">link 2</Link>
                         <LocalSwitcher className="self-center"/>
                     </div>
-                    <div className="md:hidden">
-                        <div className="flex items-center md:justify-center font-semibold self-center">
+                </div>
+                { 
+                    // * small device header
+                }
+                <div className="md:hidden p-5">
+                    <div className="flex flex-row items-center justify-between">
+                        <div className="flex items-center self-center">
                             <span>Aqua<b className="text-cyan-500">3D</b>esigner</span>
                         </div>
-                        <button
-                            onClick={() => setMenuVisible(!menuVisible)}
-                            className="text-white focus:outline-none"
-                        >
-                            <svg
-                            className="w-6 h-6"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
+                        <div className="flex flex-row items-center space-x-3">
+                            <LocalSwitcher/>
+                            <button
+                                onClick={() => setMenuVisible(!menuVisible)}
+                                className="text-white focus:outline-none"
                             >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d={menuVisible ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
-                                />
-                            </svg>
-                        </button>
+                                <svg
+                                className="w-6 h-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d={menuVisible ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+                                    />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
+                    {menuVisible && (
+                        <div className="md:hidden mt-4 space-y-2 flex flex-col">
+                            <Link href="/studio">{t('nav-create-new-layout')}</Link>
+                            <Link href="/studio">link 2</Link>
+                        </div>
+                    )}
                 </div>
-                {menuVisible && (
-                    <div className="md:hidden mt-4 space-y-2 flex flex-col">
-                        <Link href="/studio">{t('nav-create-new-layout')}</Link>
-                        <Link href="/studio">link 2</Link>
-                        <LocalSwitcher/>
-                    </div>
-                )}
             </nav>
         </header>
     )
