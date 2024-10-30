8d2be4282f04864cf8b073421fb09f298da66f20
dekrob <dekany.r123@gmail.com>
2024-10-30 22:59:18 +0100

    feat(studio): page > canvas ambientLight and directionalLight workaround

--- src/app/[locale]/studio/page.jsx
+++ src/app/[locale]/studio/page.jsx
@@ -3,17 +3,17 @@
 import { Canvas } from '@react-three/fiber'
 import classes from './page.module.css'
 import { Suspense } from 'react'
-import { Aquarium } from '@/public/3d-objects/aquarium-test/Aquarium'
+import { Aquarium } from '../../../../public/3d-objects/simple-aquarium/Aquarium.jsx'
 
 export default function Studio() {
     return (
         <>
-            <h1 className={classes.title}>Studio Page Helloszia</h1>
             <div className={classes.scene}>
-                <Canvas shadows className={classes.canvas} camera={{position: [-6, 7, 7]}}>
-                    <ambientLight color={"yellow"} intensity={0.3} />
+                <Canvas shadows className={classes.canvas} camera={{position: [0, 2, 10], fov: 50}}>
+                    <ambientLight intensity={0.5} />
+                    <directionalLight position={[5, 10, 5]} intensity={0.7} castShadow />
                     <Suspense fallback={null}>
-                        <Aquarium/>
+                        <Aquarium scale={0.1} position={[0, 0, 0]}/>
                     </Suspense>
                 </Canvas>
             </div>
