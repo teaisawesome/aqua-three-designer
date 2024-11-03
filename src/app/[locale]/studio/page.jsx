5dc0196e82b085dbfab549876897b33ebeee24ed
dekrob <dekany.r123@gmail.com>
2024-11-03 20:36:33 +0100

    feat(studio-canvas): OrbitControl added to studio base canvas

--- src/app/[locale]/studio/page.jsx
+++ src/app/[locale]/studio/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 import classes from './page.module.css'
 import { Suspense } from 'react'
 import { Aquarium } from '../../../../public/3d-objects/simple-aquarium/Aquarium.jsx'
@@ -10,6 +11,14 @@ export default function Studio() {
         <>
             <div className={classes.scene}>
                 <Canvas shadows className={classes.canvas} camera={{position: [0, 2, 10], fov: 50}}>
+                    <OrbitControls
+                        minAzimuthAngle={-Math.PI / 2}
+                        maxAzimuthAngle={Math.PI / 2}
+                        minPolarAngle={Math.PI / 6}
+                        maxPolarAngle={Math.PI - Math.PI / 2}
+                        minDistance={2}
+                        maxDistance={10}
+                    />
                     <ambientLight intensity={0.5} />
                     <directionalLight position={[5, 10, 5]} intensity={0.7} castShadow />
                     <Suspense fallback={null}>
