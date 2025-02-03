c406e17eba97e4759197a90d7dfe733de92ba6b9
Dékány Róbert Zsolt <dekany.r123@gmail.com>
2025-02-03 23:16:26 +0100

    feat(i18n): next-i18n refactored

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,9 @@
 import createMiddleware from 'next-intl/middleware';
- 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'hu'],
- 
-  // Used when no locale matches
-  defaultLocale: 'hu'
-});
- 
+import {routing} from './i18n/routing';
+
+export default createMiddleware(routing);
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(hu|en)/:path*']
+    // Match only internationalized pathnames
+    matcher: ['/', '/(en|hu)/:path*']
 };
