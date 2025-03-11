f24c6bd46a083702f8472c791c0a9f431fb89993
Dékány Róbert Zsolt <teaisawesome@users.noreply.github.com>
2025-03-11 01:33:06 +0100

    bug(zustand-store): persisted store in localstorage was not deletable, maybe zustand bug

--- src/features/studio/stores/useStudioStore.js
+++ src/features/studio/stores/useStudioStore.js
@@ -1,56 +1,64 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
 
-const useStudioStore = create(
-    persist(
-        (set) => ({
-            usedComponents: [],
-            highlightedObjectId: 0,
-            selectedObject: null,
-            index: 1,
-            transformControlMode: 'translate',
-            addUsedComponent: (title) =>
-                set((state) => ({
-                    usedComponents: [...state.usedComponents, title],
-                })),
-            addHighlightedObjectId: (id) =>
-                set((state) => ({
-                    highlightedObjectId: id
-                })),
-            incrementIndex: () =>
-                set((state) => ({
-                    index: state.index + 1
-                })),
-            toggleLockOnComponent: (componentId) =>
-                set((state) => ({
-                    usedComponents: state.usedComponents.map((component) =>
-                        component.id === componentId
-                            ? { ...component, locked: !component.locked }
-                            : component
-                    ),
-                })),
-            setSelectedObject: (object) =>
-                set((state) => ({
-                    selectedObject: object
-                })),
-            setSelectedObjectInUsedComponent: (componentId, objectRef) =>
-                set((state) => ({
-                    usedComponents: state.usedComponents.map((component) =>
-                        component.id === componentId
-                            ? { ...component, objectReference: objectRef}
-                            : component
-                    )
-                })),
-            setTransformControlMode: (mode) =>
-                set((state) => ({
-                    transformControlMode: mode
-                })),
-        }),
-        {
-            name: 'studio-storage',
-            getStorage: () => localStorage
-        }
-    )
-)
+const useStudioStore = create((set) => ({
+    usedComponents: [],
+    highlightedObjectId: 0,
+    selectedObject: null,
+    index: 1,
+    transformControlMode: 'translate',
+    addUsedComponent: (title) =>
+        set((state) => ({
+            usedComponents: [...state.usedComponents, title],
+        })),
+    addHighlightedObjectId: (id) =>
+        set((state) => ({
+            highlightedObjectId: id
+        })),
+    incrementIndex: () =>
+        set((state) => ({
+            index: state.index + 1
+        })),
+    toggleLockOnComponent: (componentId) =>
+        set((state) => ({
+            usedComponents: state.usedComponents.map((component) =>
+                component.id === componentId
+                    ? { ...component, locked: !component.locked }
+                    : component
+            ),
+        })),
+    setSelectedObject: (object) =>
+        set((state) => ({
+            selectedObject: object
+        })),
+    setSelectedObjectInUsedComponent: (componentId, objectRef) =>
+        set((state) => ({
+            usedComponents: state.usedComponents.map((component) =>
+                component.id === componentId
+                    ? { ...component, objectReference: objectRef}
+                    : component
+            )
+        })),
+    setTransformControlMode: (mode) =>
+        set((state) => ({
+            transformControlMode: mode
+        })),
+    setSelectedObjectPosition: (componentId, position, value) =>
+        set((state) => ({
+            usedComponents: state.usedComponents.map((component) => {
+                if (component.id === componentId) {
+                    const currentPosition = component.position || { x: 0, y: 0, z: 0 };
+                    return {
+                        ...component,
+                        position: {
+                            x: position === 'x' ? value : currentPosition.x,
+                            y: position === 'y' ? value : currentPosition.y,
+                            z: position === 'z' ? value : currentPosition.z,
+                        },
+                    };
+                }
+                return component;
+            })
+        }))
+}));
 
-export default useStudioStore
+export default useStudioStore;
