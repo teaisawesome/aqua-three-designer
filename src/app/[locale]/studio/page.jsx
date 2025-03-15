e61e37746526081ba3a75e5eada943f7d6007591
Dékány Róbert Zsolt <teaisawesome@users.noreply.github.com>
2025-03-15 02:27:39 +0100

    feat: try login and session on studio page

--- src/app/[locale]/studio/page.jsx
+++ src/app/[locale]/studio/page.jsx
@@ -1,8 +1,17 @@
-'use client'
-
 import StudioComponent  from '@/features/studio/components/StudioComponent.jsx'
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+import { options } from "@/app/api/auth/[...nextauth]/options";
 
-export default function Studio() {
+export default async function Studio() {
+    const session = await getServerSession(options)
+    console.log("SESSION ON SERVER:", session); // 🔹 Debug
+
+    if (!session) {
+        console.log("🔴 No session found, redirecting...");
+        //redirect('/api/auth/signin?callbackUrl=/hu/studio')
+    }
+
     return (
         <>
             <StudioComponent></StudioComponent>
