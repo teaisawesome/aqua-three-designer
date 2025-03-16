2a4f9865edf1bf9f1d8f855f610b1796bdc12242
Dékány Róbert Zsolt <teaisawesome@users.noreply.github.com>
2025-03-16 21:16:50 +0100

    refact: infopanel moved to bottom left corner

--- src/features/studio/components/canvas-tools/InfoPanel.jsx
+++ src/features/studio/components/canvas-tools/InfoPanel.jsx
@@ -1,8 +1,8 @@
-import {Info} from "lucide-react";
+import {Info} from "lucide-react"
 
 export default function InfoPanel(props) {
     return (
-        <div className={'absolute top-4 left-4'}>
+        <div className={'absolute bottom-4 left-4'}>
             <div className={'bg-sky-800 rounded-xl p-3 flex flex-row gap-4 text-white'}>
                 <Info/> Még nem csinál semmit
             </div>
