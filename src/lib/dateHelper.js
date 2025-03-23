6a4ff9bb4c31ceb646108620ecf22a71486e9f85
Dékány Róbert Zsolt <teaisawesome@users.noreply.github.com>
2025-03-23 02:14:38 +0100

    feat(helpers): dateHelper added for mongoISODateFormatting

---
+++ src/lib/dateHelper.js
@@ -0,0 +1,7 @@
+export function formatMongoISODate(isoDate) {
+    const date = new Date(isoDate)
+
+    return `${
+        date.getFullYear()}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getDate().toString().padStart(2, '0')} 
+        ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`
+}
