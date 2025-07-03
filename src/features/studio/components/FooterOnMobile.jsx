1b10a95d3e9c62b9bdfe21d36a79f94d6d28e59c
Dékány Róbert Zsolt <teaisawesome@users.noreply.github.com>
2025-07-04 00:55:24 +0200

    clear: code clear footer

--- src/features/studio/components/FooterOnMobile.jsx
+++ src/features/studio/components/FooterOnMobile.jsx
@@ -1,10 +1,10 @@
 import Properties from "@/features/studio/components/preferencebar/properties/Properties"
+import TransformControlModeSelector from "@/features/studio/components/canvas-tools/TransformControlModeSelector";
 
 export default function FooterOnMobile() {
     return (
         <div
             className={"w-full h-48 absolute bottom-0 left-0 bg-black/40 rounded-t-xl backdrop-blur-sm"}>
-            footer
             <Properties isMobile={true} />
         </div>
     )
